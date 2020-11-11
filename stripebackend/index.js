a0cd4751fd41090138566386c37fa45fc26774a4
Taylor Phillips <taylorphillips133@gmail.com>
2020-11-11 11:45:04 -0800

    begin a repo with optml image front end and a stripe basic backend

---
+++ stripebackend/index.js
@@ -0,0 +1,46 @@
+const cors = require("cors");
+const express = require("express");
+const stripe = require("stripe")("{process.env.REACT_APP_KEY}");
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+// routes
+app.get("/", (req, res) => {
+  res.send("IT WORKS AT OPTML-IMAGE");
+});
+
+app.post("/payment", (req, res) => {
+
+  const {product, token} = req.body;
+  console.log("PRODUCT", product);
+  console.log("PRICE", product.price);
+  const idempontencyKey = uuid();
+
+  return stripe.customers.create({
+    email: token.email,
+    source: token.id
+  }).then(customer => {
+    stripe.charges.create({
+      amount: product.price * 100,
+      currency: 'usd',
+      customer: customer.id,
+      receipt_email: token.email,
+      description: product.name
+    }, {idempontencyKey})
+  })
+  .then(result => res.status(200).json(result))
+  .catch(err => console.log(err))
+
+});
+
+
+// listen 
+app.listen(8282, () => console.log("LISTENING AT PORT 8282"));
